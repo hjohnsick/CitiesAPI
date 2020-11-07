cb1e9f4a7cdbf06a6db78806102250249f01d7c7
heather johnsick <hjohnsick@yahoo.com>
2020-11-07 17:51:34 -0500

    Created controllers

---
+++ CityInfo.API/Program.cs
@@ -0,0 +1,43 @@
+﻿
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using NLog;
+using NLog.Web;
+using System;
+
+namespace CityInfo.API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var logger = NLogBuilder
+                  .ConfigureNLog("nlog.config")
+                  .GetCurrentClassLogger();
+
+            try
+            {
+              
+                logger.Info("Initializing application...");
+                CreateWebHostBuilder(args).Build().Run();
+                
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex, "Application stopped because of exception.");
+                throw;
+            }
+            finally
+            {
+                NLog.LogManager.Shutdown();
+            }
+            
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .UseNLog();
+                
+    }
+}
